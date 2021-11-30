13ee4dccc3d6194392b0cdb1efe98da598eb3f8a
Sanjaykumar-11 <sanjay484641@gmail.com>
2021-11-30 15:19:13 +0530

    Updated database

--- Database/spmp.sql
+++ Database/spmp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 30, 2021 at 08:19 AM
+-- Generation Time: Nov 30, 2021 at 10:48 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.9
 
@@ -24,6 +24,94 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` varchar(20) NOT NULL,
+  `fid` varchar(20) NOT NULL,
+  `course_name` varchar(40) NOT NULL,
+  `dept` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `course`
+--
+
+INSERT INTO `course` (`course_id`, `fid`, `course_name`, `dept`) VALUES
+('18ES390 - A', 'IT007', 'DESIGN THINKING', 'IT'),
+('18ES390 - B', 'IT003', 'DESIGN THINKING', 'IT'),
+('18IT490 - A', 'IT004', 'PROJECT MANAGEMENT', 'IT'),
+('18IT490 - B', 'IT007', 'PROJECT MANAGEMENT', 'IT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_faculty`
+--
+
+CREATE TABLE `course_faculty` (
+  `fid` varchar(10) NOT NULL,
+  `mail` varchar(30) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `dept` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `course_faculty`
+--
+
+INSERT INTO `course_faculty` (`fid`, `mail`, `password`, `dept`) VALUES
+('IT003', 'faculty003@tce.edu', 'password', 'IT'),
+('IT004', 'faculty004@tce.edu', 'password', 'IT'),
+('IT007', 'faculty007@tce.edu', 'password', 'IT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `enrollment`
+--
+
+CREATE TABLE `enrollment` (
+  `regno` varchar(30) NOT NULL,
+  `course_id` varchar(30) NOT NULL,
+  `dept` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `enrollment`
+--
+
+INSERT INTO `enrollment` (`regno`, `course_id`, `dept`) VALUES
+('19IT082', '18IT490 - A', 'IT'),
+('19IT041', '18IT490 - B', 'IT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty_advisor`
+--
+
+CREATE TABLE `faculty_advisor` (
+  `fid` varchar(10) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `dept` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `faculty_advisor`
+--
+
+INSERT INTO `faculty_advisor` (`fid`, `email`, `password`, `dept`) VALUES
+('CSE011', 'facultycse011@tce.edu', 'password', 'CSE'),
+('IT001', 'faculty001@tce.edu', 'password', 'IT'),
+('IT010', 'faculty010@tce.edu', 'password', 'IT'),
+('MA001', 'facultymech001@tce.edu', 'password', 'MECH');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `project`
 --
 
@@ -40,15 +128,90 @@ CREATE TABLE `project` (
 INSERT INTO `project` (`project_id`, `project_name`, `team_id`) VALUES
 (1, 'Clinic Appointment System', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `regno` varchar(8) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `password` varchar(300) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `dept_id` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`regno`, `mail`, `password`, `name`, `dept_id`) VALUES
+('19IT027', 'eniyan@student.tce.edu', 'password', 'ENIYAN S', 'IT'),
+('19IT040', 'jeswinw@student.tce.edu', 'password', 'JESWIN W', 'IT'),
+('19IT082', 'sanjaykumars@student.tce.edu', 'password', 'SANJAYKUMAR S', 'IT');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+
+CREATE TABLE `team` (
+  `team_id` int(30) NOT NULL,
+  `team_members` varchar(30) NOT NULL,
+  `course_id` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `team`
+--
+
+INSERT INTO `team` (`team_id`, `team_members`, `course_id`) VALUES
+(1, '19IT082, 19IT041, 19IT040', '18ES390 - A');
+
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `course_faculty`
+--
+ALTER TABLE `course_faculty`
+  ADD PRIMARY KEY (`fid`),
+  ADD UNIQUE KEY `mail` (`mail`);
+
+--
+-- Indexes for table `faculty_advisor`
+--
+ALTER TABLE `faculty_advisor`
+  ADD PRIMARY KEY (`fid`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `project`
 --
 ALTER TABLE `project`
   ADD PRIMARY KEY (`project_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`regno`),
+  ADD UNIQUE KEY `mail` (`mail`);
+
+--
+-- Indexes for table `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`team_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -59,6 +222,12 @@ ALTER TABLE `project`
 --
 ALTER TABLE `project`
   MODIFY `project_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `team`
+--
+ALTER TABLE `team`
+  MODIFY `team_id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
